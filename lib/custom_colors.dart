3be964ba48afb78636c644ef2f46512fc0bbad2d
victorimannuel21 <victorimannuel21@gmail.com>
2023-07-19 12:08:32 +0700

    completed bwa course and added splash screen

---
+++ lib/custom_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+// Colors hex codes
+Color darkBlue = Color(0XFF2c2c54);
+Color darkSilver = Color(0XFF84817a);
---
+++ lib/font_style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:gpdi_betlehem_apps/custom_colors.dart';
+
+TextStyle mainHeader = TextStyle(
+  fontSize: 26,
+  color: darkBlue,
+  fontFamily: "Poppins",
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle subHeader = TextStyle(
+  fontSize: 16,
+  color: darkSilver,
+  fontFamily: "Poppins",
+  fontWeight: FontWeight.w300,
+);
